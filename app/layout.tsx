258fb291729a9d735ae40000066ee2016b4c0ddd
AndreiRech <andreirech22@outlook.com>
2026-04-07 09:21:34 -0300

    feat: add the correct fonts and change color values

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Zilla_Slab } from 'next/font/google';
 
 import '@/scss/globals.scss';
 import { Client } from './client';
@@ -7,14 +8,21 @@ export const metadata: Metadata = {
     title: 'Amores Fati',
 };
 
+const zillaSlab = Zilla_Slab({
+    weight: ['300', '400', '500', '600', '700'],
+    style: ['normal', 'italic'],
+    subsets: ['latin'],
+    variable: '--font-zilla-next',
+});
+
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" suppressHydrationWarning>
-            <body suppressHydrationWarning className="custom-body antialiased">
+        <html lang='en' suppressHydrationWarning className={zillaSlab.variable}>
+            <body suppressHydrationWarning className='custom-body antialiased'>
                 <Client>{children}</Client>
             </body>
         </html>
