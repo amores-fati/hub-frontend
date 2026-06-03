49b6b816fd5c987c00d873bd23548c31b6a4777f
Paola Lopes <paolaanacletolopes@gmail.com>
2026-06-03 00:49:30 -0300

    Adicionando na navbar do administrador empresas e vagas

--- providers/Route/PagesConfiguration.tsx
+++ providers/Route/PagesConfiguration.tsx
@@ -7,6 +7,7 @@ import {
     MenuBook as MenuBookIcon,
     Person as PersonIcon,
     Work as WorkIcon,
+    Business as BusinessIcon
 } from '@mui/icons-material';
 import React, { JSX } from 'react';
 
@@ -121,6 +122,22 @@ export const PAGES: Page[] = [
         name: 'Perfil',
         icon: <PersonIcon className='sidebar-icon' />,
     },
+    {
+    path: '/admin/empresas',
+    navbarEnabled: true,
+    requireAuth: true,
+    requireRoles: [UserRole.ADMIN],
+    name: 'Gestão de Empresas',
+    icon: <BusinessIcon className='sidebar-icon' />,
+    },
+    {
+    path: '/admin/vagas',
+    navbarEnabled: true,
+    requireAuth: true,
+    requireRoles: [UserRole.ADMIN],
+    name: 'Gestão de Vagas',
+    icon: <WorkIcon className='sidebar-icon' />,
+    },
 ];
 
 export type NavItem = {
@@ -143,6 +160,8 @@ export const NAVIGATION_MAP: Record<string, NavItem[]> = {
             icon: <AssignmentIndIcon />,
             expectedPath: '/admin/curriculos',
         },
+        { title: 'Empresas', icon: <BusinessIcon />, expectedPath: '/admin/empresas' },
+        { title: 'Vagas', icon: <WorkIcon />, expectedPath: '/admin/vagas' },
     ],
     [UserRole.STUDENT]: [
         {
