e2fe48b621869d4e843e824dca225ee7c990942a
Hartur Kunzler <hartur.kunzler@gmail.com>
2026-06-21 10:24:56 -0300

    Ajustes cursos (torna limpavel filtro)

--- app/admin/cursos/AdminCourses.tsx
+++ app/admin/cursos/AdminCourses.tsx
@@ -530,6 +530,7 @@ function AdminStudents() {
                                 value={modality}
                                 onChange={(option) => setModality(option)}
                                 isSearchable
+                                isClearable
                             />
                         </div>
                     </div>
